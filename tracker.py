fdce043fa87c3b31b1098804f1bfe4dcda59d5c2
Egbert Bouman <ebouman@gmail.com>
2014-09-07 20:20:19 +0200

    Add TCP tracker support

--- tracker.py
+++ tracker.py
@@ -1,10 +1,33 @@
 import sys
+import time
 import signal
 import struct
 import socket
 import select
-import random
 import argparse
+
+
+# Source bencode function: https://gist.github.com/pyropeter/642505
+def bencode(obj):
+    if isinstance(obj, int):
+        return "i" + str(obj) + "e"
+
+    if isinstance(obj, str):
+        return str(len(obj)) + ":" + obj
+
+    if isinstance(obj, list):
+        res = "l"
+        for elem in obj:
+            res += bencode(elem)
+        return res + "e"
+
+    if isinstance(obj, dict):
+        res = "d"
+        for key in sorted(obj.keys()):
+            res += bencode(key) + bencode(obj[key])
+        return res + "e"
+
+    raise Exception, "Unknown object: %s" % repr(obj)
 
 
 class UDPTracker(object):
@@ -31,7 +54,7 @@ class UDPTracker(object):
             print "UDP tracker: client sent connection request with invalid connection_id ", connection_id
             return
 
-        # Respond to connection request with an error message, and the client IP as error message.
+        # Respond to connection request with an error message containing the client IP.
         response = struct.pack('!II', 3, transaction_id) + 'Your IP is %s:%d' % addr
         self.socket.sendto(response, addr)
         print "UDP tracker: sending response", response.encode('hex'), 'to %s:%d' % addr
@@ -43,15 +66,27 @@ class TCPTracker(object):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setblocking(0)
         self.socket.bind(('0.0.0.0', port))
-        print 'TCP tracker: listening %d' % port
+        self.socket.listen(1)
+        print 'TCP tracker: listening on port %d' % port
 
     def handle_request(self):
-        # TODO...
-        pass
+        conn, addr = self.socket.accept()
+        request = conn.recv(1024)
+        print "TCP tracker: received request", request.split('\n')[0]
+
+        bencoded_dict = bencode({'failure reason': 'Your IP is %s:%d' % addr})
+        headers = 'HTTP/1.1 200 OK\n'
+        headers += 'Date: ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()) + '\n'
+        headers += 'Server: torrent-ip-checker\n'
+        headers += 'Connection: close\n\n'
+        response = headers + bencoded_dict
+        conn.send(response)
+        print "TCP tracker: sending response", response.split('\n')[0], 'to %s:%d' % addr
+        conn.close()
 
 
 def main(argv):
-    parser = argparse.ArgumentParser(description='Runs a fake tracker, and only reports back the ' +
+    parser = argparse.ArgumentParser(description='Runs a fake tracker that only reports back the ' +
                                                  'IP of the connecting client.')
 
     try:
